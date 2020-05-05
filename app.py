a7c4dce9653fc2bb0845f8e6c626c249a1e99190
Cindy Tsou <cindytsou@cindys-air.gateway.ht.net>
2020-05-04 16:52:11 -1000

    First commit

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ app = Flask(__name__)
 
 #static route
 @app.route("/")
-def test():
+def hello():
     return render_template("index.html")
 
 @app.route("/1006")
-def test123():
-    #return "<p>This is my <b>1006</b> website! </p>"
-    return render_template("1006.html")
+def homepage():
+    return render_template("1006.html") 
 
 #start the server
 if __name__ == "__main__":
